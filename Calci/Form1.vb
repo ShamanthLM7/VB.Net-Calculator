2f7b8f7d2a0c60c5375b2e06a89a90d380af3605
ShamanthLM7 <naikshamanth92@gmail.com>
2022-09-11 21:53:50 +0530

    first commit

---
+++ Calci/Form1.vb
@@ -0,0 +1,154 @@
+﻿Public Class Form1
+    Dim FirstNum As Decimal
+    Dim secondNum As Decimal
+    Dim operations As Integer
+    Dim op_select As Boolean = False
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "1"
+        Else
+            TextBox1.Text = "1"
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "2"
+        Else
+            TextBox1.Text = "2"
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "3"
+        Else
+            TextBox1.Text = "3"
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "4"
+        Else
+            TextBox1.Text = "4"
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "5"
+        Else
+            TextBox1.Text = "5"
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "6"
+        Else
+            TextBox1.Text = "6"
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "7"
+        Else
+            TextBox1.Text = "7"
+        End If
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "8"
+        Else
+            TextBox1.Text = "8"
+        End If
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "9"
+        Else
+            TextBox1.Text = "9"
+        End If
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+
+        TextBox1.Text += "0"
+
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        If op_select = True Then
+            secondNum = TextBox1.Text
+            If operations = 1 Then
+                TextBox1.Text = FirstNum + secondNum
+            ElseIf operations = 2 Then
+                TextBox1.Text = FirstNum - secondNum
+            ElseIf operations = 3 Then
+                TextBox1.Text = FirstNum * secondNum
+            Else
+                If secondNum = "0" Then
+                    TextBox1.Text = "Error"
+                Else
+                    TextBox1.Text = FirstNum / secondNum
+                End If
+            End If
+            op_select = False
+        End If
+
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        If Not (TextBox1.Text.Contains(".")) Then
+            TextBox1.Text += "."
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        FirstNum = TextBox1.Text
+        TextBox1.Text = "0"
+        op_select = True
+        operations = 1
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        FirstNum = TextBox1.Text
+        TextBox1.Text = "0"
+        op_select = True
+        operations = 2
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        FirstNum = TextBox1.Text
+        TextBox1.Text = "0"
+        op_select = True
+        operations = 3
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        FirstNum = TextBox1.Text
+        TextBox1.Text = "0"
+        op_select = True
+        operations = 4
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        TextBox1.Text = "0"
+
+    End Sub
+End Class
